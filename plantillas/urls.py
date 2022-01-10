f78e1aa8cbb28ec93f81e4b93e38022834fedd97
kaxpe-lee <fmartinez@work-art.net>
2022-01-10 19:12:48 +0100

    xxs

--- plantillas/urls.py
+++ plantillas/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
+from plantillas.views import saludo, inicio, some_view
 
 from . import views
 
 app_name = 'plantillas'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('pdf', some_view),
+    path('', inicio),
 
     path('<int:id>/', views.detail, name='detail'),
 
     path('<int:id>/results/', views.results, name='results'),
 
     path('<int:id>/vote/', views.vote, name='vote'),
+
+    path('saludo/', saludo),
 ]
--- plantillas/views.py
+++ plantillas/views.py
@@ -1,11 +1,50 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect, FileResponse
 # from django.template import loader
 from django.urls import reverse
 
 from .models import Plantilla
+import datetime
+
+import io
+from reportlab.pdfgen import canvas
 
 # Create your views here.
+
+
+def some_view(request):
+    # Create a file-like buffer to receive PDF data.
+    buffer = io.BytesIO()
+
+    # Create the PDF object, using the buffer as its "file."
+    p = canvas.Canvas(buffer)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawString(100, 100, "Hello world.")
+    p.drawString(0, 400, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper urna et erat aliquet sollicitudin. Morbi faucibus nibh sodales, convallis ante eget, venenatis ex. Vivamus interdum ultricies dapibus. Nam tempus, turpis a porta aliquet, est ex pulvinar diam, sit amet convallis turpis ex a metus. Praesent tincidunt hendrerit pellentesque. Pellentesque quis feugiat odio, a varius augue. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc pharetra, enim vel interdum pharetra, magna nunc malesuada dui, vitae tincidunt est diam a odio. Nulla vitae maximus mi. Nunc eget felis tortor. Nulla rhoncus rutrum felis, a placerat magna lacinia at. Curabitur eget urna metus. In accumsan ipsum in diam aliquet euismod. Vivamus ante nunc, pharetra et. ")
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+
+    # FileResponse sets the Content-Disposition header so that browsers
+    # present the option to save the file.
+    buffer.seek(0)
+    return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
+
+
+def saludo(request):
+    fecha = datetime.datetime.now().year
+    texto = '<h3>Hola estamos a %s </h3>' % fecha
+    return HttpResponse(texto)
+
+
+def inicio(request):
+    context = {
+
+    }
+    return render(request, 'plantillas/base.html', context)
 
 
 def index(request):
